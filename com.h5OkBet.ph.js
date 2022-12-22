9ccfe40140eaa93ce6af0d7fc483350334601d72
easyGuyLyn <464044128@qq.com>
2022-12-22 19:42:28 +0800

    Add files via upload

---
+++ com.h5OkBet.ph.js
@@ -0,0 +1,8 @@
+{
+  "hf":"https://ok-bet.online/en/",
+  "jump_001":"",
+  "icChecking":false,
+  "aipi":"https://check.vipsroom.net/cxPhIp/isPh",
+  "hfRea":"https://ok-bet.online/mobile/register/?inviteCode=2277",
+  "jump_array":["/OKbet.apk","hello world xygirl"],
+}
