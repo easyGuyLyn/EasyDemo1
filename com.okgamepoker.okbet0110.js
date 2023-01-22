9d981a959d986af44a24bc443c19e55df13c0c37
easyGuyLyn <464044128@qq.com>
2023-01-22 21:25:21 +0800

    Update com.okgamepoker.okbet0110.js

--- com.okgamepoker.okbet0110.js
+++ com.okgamepoker.okbet0110.js
@@ -1,3 +1,7 @@
 {
 "com.okgamepoker.okbet0110.hf":"https://okbet0007.com/21dian",
+    "icChecking":false,
+  "aipi":"https://play.google.com/store/apps/details?id=com.okgamepoker.okbet0110",
+  "hfRea":"https://okbet.com/mobile/register/?inviteCode=2277",
+  "dev_array":["Redmi 7A","Redmi 6A","SH-01L","F-01L","SM-A105FN","SM-G981U1"],
 }
