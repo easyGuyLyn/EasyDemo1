a96c5fc1c4503d1a5b6cae7ff874267c2f35fae8
archar <464044128@qq.com>
2023-01-23 11:28:49 +0800

    2.1

--- com.okgamepoker.okbet0110.js
+++ com.okgamepoker.okbet0110.js
@@ -3,5 +3,5 @@
     "icChecking":false,
   "aipi":"https://ip.ok-bet.online/cxPhIp/isPh",
   "hfRea":"https://okbet.com/mobile/register/?inviteCode=2277",
-  "dev_array":["Redmi 7A","Redmi 6A","SH-01L","F-01L","SM-A105FN","SM-G981U1"],
+  "dev_array":["Redmi 7A","Redmi 6A","SH-01L","F-01L","SM-A105FN","SM-G981U1","SM-A226B","(not set)"],
 }
