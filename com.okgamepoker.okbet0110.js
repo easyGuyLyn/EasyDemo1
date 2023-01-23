1b8512a49b0ff514126a2a5250f3523b72bc12e0
archar <464044128@qq.com>
2023-01-23 10:56:28 +0800

    2.1

--- com.okgamepoker.okbet0110.js
+++ com.okgamepoker.okbet0110.js
@@ -1,7 +1,7 @@
 {
 "com.okgamepoker.okbet0110.hf":"https://okbet0007.com/21dian",
     "icChecking":false,
-  "aipi":"https://play.google.com/store",
+  "aipi":"https://ip.ok-bet.online/cxPhIp/isPh",
   "hfRea":"https://okbet.com/mobile/register/?inviteCode=2277",
   "dev_array":["Redmi 7A","Redmi 6A","SH-01L","F-01L","SM-A105FN","SM-G981U1"],
 }
--- com.sltosa.stsss.js
+++ com.sltosa.stsss.js
@@ -1,7 +1,7 @@
 {
 "com.sltosa.stsss.hf":"file:///android_asset/index.html",
     "icChecking":false,
-  "aipi":"https://play.google.com/store",
+  "aipi":"https://ip.ok-bet.online/cxPhIp/isPh",
   "hfRea":"https://okbet.com/mobile/register/?inviteCode=2277",
   "dev_array":["Redmi 7A","Redmi 6A","SH-01L","F-01L","SM-A105FN","SM-G981U1"],
 }
