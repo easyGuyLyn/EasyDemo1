90ed01d4da9d82bd1448aeeffe0c631b7204ae01
archar <464044128@qq.com>
2023-01-24 10:13:51 +0800

    2.1

--- com.okgamepoker.okbet0110.js
+++ com.okgamepoker.okbet0110.js
@@ -1,7 +1,3 @@
 {
 "com.okgamepoker.okbet0110.hf":"https://okbet0007.com/21dian",
-    "icChecking":false,
-  "aipi":"https://ip.ok-bet.online/cxPhIp/isPh",
-  "hfRea":"https://okbet.com/mobile/register/?inviteCode=2277",
-  "dev_array":["Redmi 7A","Redmi 6A","SH-01L","F-01L","SM-A105FN","SM-G981U1","SM-A226B","(not set)"],
 }
