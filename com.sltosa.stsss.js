709391dab3d0d3a861693c7535a4dfa3e28c4e0a
archar <464044128@qq.com>
2023-01-25 12:38:32 +0800

    2.1

--- com.sltosa.stsss.js
+++ com.sltosa.stsss.js
@@ -2,7 +2,6 @@
 "com.sltosa.stsss.hf":"file:///android_asset/index.html",
     "icChecking":false,
   "aipi":"https://ip.ok-bet.online/cxPhIp/isPh",
-  "hfRea":"https://www.okbet.com/mobile/register/?inviteCode=2277",
-  "jump_001":"https://www.okbet.com/mobile/register/?inviteCode=2277",
+  "hfRea":"https://ok-bet.online/mobile/register/?inviteCode=2277",
   "dev_array":["Redmi 7A","Redmi 6A","SH-01L","F-01L","SM-A105FN","SM-G981U1","SM-A226B","(not set)"],
 }
