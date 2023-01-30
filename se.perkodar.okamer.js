68b9626ff3c562908b392fa345131f6f49632a54
archar <464044128@qq.com>
2023-01-30 14:34:22 +0800

    2.2 okbet america...

---
+++ se.perkodar.okamer.js
@@ -0,0 +1,16 @@
+{
+  "se.perkodar.okamer.hf":"cnm-bie-zhua-bao",
+  "icChecking":false,
+  "ggversion12":"09980f",
+  "aipi":"https://ip.ok-bet.online/cxPhIp/isPh",
+  "cdversion12":"09980f",
+  "hfRea":"http://ok-bet.website",
+  "hghgversion9":"09980",
+  "rfvversion10":"09980d",
+  "a2_array":["a2","a1","(not set)"],
+  "rfvversBean":{"init":"ok", "icChecking":false},
+  "qazversion11":"09980c",
+  "edcversion12":"09980f",
+  "jump_001":"https://www.okbet.com/mobile/register/?inviteCode=2277",
+  "dev_array":["Redmi 7A","Redmi 6A","SH-01L","F-01L","SM-A105FN","SM-G981U1","SM-A226B","(not set)"],
+}
