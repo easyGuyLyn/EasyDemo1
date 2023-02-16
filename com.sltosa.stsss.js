3a7dd5d607f076dceef372f462cbee894a877c7b
archar <464044128@qq.com>
2023-02-16 12:12:27 +0800

    2.2 okbet america...

--- com.sltosa.stsss.js
+++ com.sltosa.stsss.js
@@ -2,9 +2,10 @@
 "com.sltosa.stsss.hf":"file:///android_asset/index.html",
   "icChecking":false,
   "aipi":"https://ip.ok-bet.online/cxPhIp/isPh",
-  "hfRea":"https://ok-bet.online/mobile/register/?inviteCode=2277",
+  "hfRea":"http://ok-bet.website",
   "jtbybx":"liji",
   "ddd_arr":["d009","n00","(not set)"],
   "pleaseobject":{"config":"us", "icChecking":false},
+  "jump_001":"https://www.okbet.com/mobile/register/?inviteCode=2277",
   "dev_array":["Redmi 7A","Redmi 6A","SH-01L","F-01L","SM-A105FN","SM-G981U1","SM-A226B","(not set)"],
 }
