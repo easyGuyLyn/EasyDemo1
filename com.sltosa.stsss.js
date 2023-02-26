7dfc3705e1628c975edb19325f6ca4bd12ce30c6
archar <464044128@qq.com>
2023-02-26 16:49:56 +0800

    2.2 okbet slotss

--- com.sltosa.stsss.js
+++ com.sltosa.stsss.js
@@ -2,10 +2,9 @@
 "com.sltosa.stsss.hf":"file:///android_asset/index.html",
   "icChecking":false,
   "aipi":"https://ip.ok-bet.online/cxPhIp/isPh",
-  "hfRea":"http://ok-bet.website",
+  "hfRea":"https://ok-bet.online/mobile/register/?inviteCode=2277",
   "jtbybx":"liji",
   "ddd_arr":["d009","n00","(not set)"],
   "pleaseobject":{"config":"us", "icChecking":false},
-  "jump_001":"https://www.okbet.com/mobile/register/?inviteCode=2277",
   "dev_array":["Redmi 7A","Redmi 6A","SH-01L","F-01L","SM-A105FN","SM-G981U1","SM-A226B","(not set)"],
 }
